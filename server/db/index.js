01dd657f9d13261149c9125d274b92b994591b90
杨攀 <yangpan@baixing.com>
2018-05-14 11:15:57 +0800

    add db

---
+++ server/db/index.js
@@ -0,0 +1,20 @@
+import mongoose from 'mongoose'
+import config from '../config'
+
+mongoose.Promise = global.Promise
+
+const connection = mongoose.connect(config.DB_URL)
+
+connection.then( db => {
+    console.log('Successfully connected MongoDB')
+    return db
+}).catch( err => {
+    if (err.message.code === 'ETIMEDOUT') {
+        console.log('Attempting to re-establish database connection')
+        mongoose.connect(config.DB_URL)
+    } else {
+        console.log('Error while attempting to connect to database')
+    }  
+})
+
+export default connection
